39aef3bde0cffe3cfede7eae06d33f4f9aebafb6
gyubuk <g1370200@gmail.com>
2026-04-08 12:18:10 +0900

    feat : Tailwind CDN → 빌드 파일로 교체

---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    './apps/**/templates/**/*.html',
+    './templates/**/*.html',
+    './apps/**/static/**/*.js',
+    './static/**/*.js',
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
